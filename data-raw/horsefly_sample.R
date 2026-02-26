ecd584a3c8928533fdf3ee615b7d31e792c412ec
Laura Alsip <lalsip@mts.net>
2026-02-26 13:55:01 -0600

    add data

---
+++ data-raw/horsefly_sample.R
@@ -0,0 +1,14 @@
+## code to prepare `horsefly_sample` dataset goes here
+# data-raw/horsefly_sample.R
+# Run this script once to create data/horsefly_sample.rda.
+
+horsefly_sample <- data.frame(
+  set_number = c(1L, 2L, 3L, 4L, 5L),
+  catch = c(3L, 0L, 5L, 2L, 4L),
+  effort = c(1L, 1L, 1L, 1L, 1L),
+  stringsAsFactors = FALSE
+)
+
+usethis::use_data(horsefly_sample, overwrite = TRUE)
+
+source("data-raw/horsefly_sample.R")
